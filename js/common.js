6a4b73a5ad2ce37fa29edb9b3a14651b6bd851fa
oomphoomph <rvelichko95@gmail.com>
2016-08-19 16:07:27 +0600

    initial commit

---
+++ js/common.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+	function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+});
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
+	$(".top_text p").animated("fadeInUp", "fadeOutDown");
+
+$(".toggle_mnu").click(function() {
+		$(".top_mnu ul").slideToggle();
+	});
+
+
+}); 
